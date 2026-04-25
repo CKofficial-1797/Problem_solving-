1488d53809add4be85a739b72c552b7ffac406b5
CKofficial-1797 <sanjaygorain1797@gmail.com>
2026-04-25 22:35:08 +0530

    solved

--- miscellaneus/M.java
+++ miscellaneus/M.java
@@ -17,18 +17,32 @@ public class M
             coins[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(coins);
-        int ans = f(n-1 , x);
-        System.out.println(ans == L ?-1 : ans);
+        // int ans = f(n-1 , x);
+        int[] dp = new int[x+1];
+        Arrays.fill(dp,L);
+        dp[0]=0;
+        for(int i =n-1; i>=0 ; i--){
+            int[] curr = new int[x+1];
+            Arrays.fill(curr,L);
+            curr[0]=0;
+            for(int sum = 1 ; sum<= x; sum++){
+                if(sum-coins[i]<0)continue;
+                int take = 1+ Math.min(curr[sum-coins[i]] ,dp[sum-coins[i]]);
+                curr[sum] = Math.min(take , dp[sum]);
+            }
+            dp=curr;
+        }
+        System.out.println(dp[x] == L ?-1 : dp[x]);
 	}
 	private static final int L = 1_000_000_000;
 	private static int[]coins;
-	private static int f(int id, int sum){
-	    if(sum<=0){
-	        if(sum==0) return 0;
-	        return L;
-	    }
-	    if(id<0)return L;
-	    int take = 1+ Math.min(f(id,sum-coins[id]),f(id-1,sum-coins[id]));
-	    return Math.min(take , f(id-1,sum));
-	}   
+// 	private static int f(int id, int sum){
+// 	    if(sum<=0){
+// 	        if(sum==0) return 0;
+// 	        return L;
+// 	    }
+// 	    if(id<0)return L;
+// 	    int take = 1+ Math.min(f(id,sum-coins[id]),f(id-1,sum-coins[id]));
+// 	    return Math.min(take , f(id-1,sum));
+// 	}   
 }
